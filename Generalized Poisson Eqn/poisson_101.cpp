ab30096398df013ef0d0bd39c7f81892b674e345
spranav1205 <spranav1205@gmail.com>
2026-06-03 15:17:09 +0530

    all done

--- Generalized Poisson Eqn/poisson_101.cpp
+++ Generalized Poisson Eqn/poisson_101.cpp
@@ -18,13 +18,16 @@
 
 #include <fstream>
 #include <iostream>
+#include <cmath>
+#include <map>
+#include <vector>
 
 using namespace dealii;
 
 #define r_c 0.5 // Less than half the distance between the two nuclei
-#define r_c_squared = r_c * r_c
-#define r_c_8 = r_c_squared * r_c_squared * r_c_squared * r_c_squared
-#define PI = 3.14159265358979323846
+#define r_c_squared r_c * r_c
+#define r_c_8 r_c_squared * r_c_squared * r_c_squared * r_c_squared
+#define PI 3.14159265358979323846
 
 
 template <int dim>
@@ -48,8 +51,8 @@ class SmearedCharge : public Function<dim> // Inherit from Function class
 
             if(r <= r_c)
             {
-                double n = -21 * (r - r_c) * (r - r_c) * (r - r_c) * (6*r_squared + 3*r*r_c + r_c_squared);
-                double d = 5*PI*r_c_8;
+                double n = -21.0 * (r - r_c) * (r - r_c) * (r - r_c) * (6.0 * r_squared + 3.0 * r * r_c + r_c_squared);
+                double d = 5.0 * PI * r_c_8;
                 return charge*n / d;
             }
             else
@@ -77,6 +80,9 @@ class CoullombPotential : public Function<dim> // Inherit from Function class
 
             double r = std::sqrt(r_squared);
 
+            if (r < 1e-12)
+                return charge / 1e-12;
+
             return charge / r;
         }
 };
@@ -86,13 +92,16 @@ class Poisson
 {
     public:
         Poisson(int degree);
-        void run();
+        void run(int refinement_level, bool verbose = false);
+
+        const Vector<double> &get_solution() const;
+        const Vector<double> &get_rhs() const;
 
     private:
         void setup_system(int refinement_level);
-        void assemble_system(Function<dim> &forcing_function, Function<dim> &boundary_condition, const double charge);
-        void solve();
-        void output_results() const;
+        void assemble_system(SmearedCharge<dim> &forcing_function, CoullombPotential<dim> &boundary_condition);
+        void solve(bool verbose);
+        void output_results(bool verbose) const;
 
         // Mesh, finite element (basis functions), and dof handler (dof to mesh mapping)
         Triangulation<dim> triangulation;
@@ -104,12 +113,26 @@ class Poisson
         SparseMatrix<double> system_matrix;
         Vector<double> solution;
         Vector<double> system_rhs;
+        Vector<double> system_rhs_before;
 };
 
 template <int dim>
 Poisson<dim>::Poisson(int degree) : fe(degree), dof_handler(triangulation)
 {
     
+}
+
+// Returns pointer to solution vector
+template <int dim>
+const Vector<double>& Poisson<dim>::get_solution() const
+{
+    return solution;
+}
+
+template <int dim>
+const Vector<double>& Poisson<dim>::get_rhs() const
+{
+    return system_rhs_before;
 }
 
 template <int dim>
@@ -119,22 +142,30 @@ void Poisson<dim>::setup_system(int refinement_level)
 
     GridGenerator::hyper_cube(triangulation, -1, 1);
     triangulation.refine_global(refinement_level); // n_cells = 2^(dim*refinement_level)
+    // Cells per direction TODO
+
     dof_handler.distribute_dofs(fe); // Assigns dofs to the mesh vertices, edges, faces, and cells
     
     // Make big matrix -> see where the non-zero entries are -> make sparse matrix with non-zero entries only
     DynamicSparsityPattern dsp(dof_handler.n_dofs(), dof_handler.n_dofs());
-    SparsityTools::distribute_dofs(dof_handler, dsp);
-    system_matrix.reinit(dsp);
+    DoFTools::make_sparsity_pattern(dof_handler, dsp);
+    
+    sparsity_pattern.copy_from(dsp);
+    system_matrix.reinit(sparsity_pattern);
 
+    solution.reinit(dof_handler.n_dofs());
+    system_rhs.reinit(dof_handler.n_dofs());
+    system_rhs_before.reinit(dof_handler.n_dofs());
 }
 
 template <int dim>
-void Poisson<dim>::assemble_system(Function<dim> &forcing_function, Function<dim> &boundary_condition, const double charge) // TODO: Pass list of atoms
+void Poisson<dim>::assemble_system(SmearedCharge<dim> &forcing_function, CoullombPotential<dim> &boundary_condition)
 {
     QGauss<dim> quadrature_formula(fe.degree + 1); // Quadrature
     FEValues<dim> fe_values(fe, quadrature_formula, update_values | update_gradients | update_JxW_values | update_quadrature_points); 
     const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
     const unsigned int n_q_points = quadrature_formula.size();
+    (void) n_q_points;
 
     // Local cell matrix and rhs vector
     FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
@@ -143,15 +174,16 @@ void Poisson<dim>::assemble_system(Function<dim> &forcing_function, Function<dim
     // Local array mapping local DoF index to global matrix index
     std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell); 
 
-    forcing_function.charge = charge; 
-    boundary_condition.charge = charge;
-
+    // a_ij =  summation ( grad phi_i * grad phi_j * jacobian * quadrature weight )
+    // b_i =  summation ( f(x) * phi_i * jacobian * quadrature weight )
+    // phi_i and phi_j are the basis functions
     for (const auto &cell : dof_handler.active_cell_iterators())
     {
         cell_matrix = 0;
         cell_rhs = 0;
 
-        fe_values.reinit(cell); // Updates values for current cell geometry
+        fe_values.reinit(cell); // When different cells have different geometry
+
         // Pointer to quadrature points for this cell
         const std::vector<Point<dim>> &quadrature_points = fe_values.get_quadrature_points();
 
@@ -184,6 +216,9 @@ void Poisson<dim>::assemble_system(Function<dim> &forcing_function, Function<dim
         }
     }
 
+    // Important!
+    system_rhs_before = system_rhs;
+
     // USEFULL
     std::map<types::global_dof_index, double> boundary_values;
     
@@ -192,19 +227,22 @@ void Poisson<dim>::assemble_system(Function<dim> &forcing_function, Function<dim
 }
 
 template <int dim>
-void Poisson<dim>::solve()
+void Poisson<dim>::solve(bool verbose)
 {
     SolverControl solver_control(1000, 1e-12 * system_rhs.l2_norm());
-    SolverCG<> solver(solver_control);
+    SolverCG<Vector<double>> solver(solver_control);
 
     // Initially no preconditioner
-    cg_solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+    solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
 
-    std::cout << "   " << solver_control.last_step() << " CG iterations needed to obtain convergence." << std::endl;
+    if(verbose)
+    {
+        std::cout << "   " << solver_control.last_step() << " CG iterations needed to obtain convergence." << std::endl;
+    }
 }
 
 template <int dim>
-void Poisson<dim>::output_results() const
+void Poisson<dim>::output_results(bool verbose) const
 {
     DataOut<dim> data_out;
     data_out.attach_dof_handler(dof_handler);
@@ -213,5 +251,81 @@ void Poisson<dim>::output_results() const
     std::ofstream output("poisson-solution.vtu");
     data_out.write_vtu(output);
     
-    std::cout << "  Results written to poisson-solution.vtu" << std::endl;
+    if (verbose)
+    {
+        std::cout << "  Results written to poisson-solution.vtu" << std::endl;
+    }
 }
+
+template <int dim>
+void Poisson<dim>::run(int refinement_level, bool verbose)
+{
+    if (verbose)
+    {
+        std::cout << "Setting up system with refinement level " << refinement_level << "..." << std::endl;
+    }
+
+    setup_system(refinement_level); // Now dynamically controlled!
+
+    SmearedCharge<dim> forcing_term;
+    CoullombPotential<dim> boundary_term;
+
+    forcing_term.charge = 3.0; 
+    boundary_term.charge = 3.0;
+
+
+    if (verbose)
+    {
+        std::cout << "Assembling matrix and vectors..." << std::endl;
+    }
+
+    assemble_system(forcing_term, boundary_term);
+
+    if(verbose)
+    {
+        std::cout << "Solving linear system via CG..." << std::endl;
+    }
+
+    solve(verbose);
+
+    if(verbose)
+    {
+        std::cout << "Generating output files..." << std::endl;
+    }
+
+    output_results(verbose);
+    
+}
+
+double electrostatic_energy(const Vector<double> &sol, const Vector<double> &rhs)
+{
+    double I =  sol * rhs; 
+
+    return -0.5 * I;
+}
+
+int main()
+{
+    int degree = 6;
+
+    for(int refinement_level = 1; refinement_level <= 5; ++refinement_level)
+    {
+        // std::cout << "Running Poisson solver with refinement level " << refinement_level << "..." << std::endl;
+        Poisson<3> poisson_problem(degree);
+        poisson_problem.run(refinement_level);
+
+        const Vector<double> &solution = poisson_problem.get_solution();
+        const Vector<double> &rhs = poisson_problem.get_rhs();
+
+        //Total charge
+        cout<< "Total Charge: " << rhs.l1_norm() << std::endl;
+
+        // Debug
+        cout<< "Solution vector size: " << solution.size() << std::endl;
+        cout<< "RHS vector size: " << rhs.size() << std::endl;
+
+        double energy = electrostatic_energy(solution, rhs);
+        std::cout << "Electrostatic energy at refinement level " << refinement_level << ": " << energy << std::endl;
+    }
+}
+
